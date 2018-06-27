eb4e390752af89cf1b0db5be4d53069631f1496e
adrien Bodineau <adrien.bodineau@gmail.com>
2018-06-27 23:29:46 +0200

    mise en place de la rejouabilité

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,18 @@ fn creation_de_la_valeur_a_deviner(min: usize, max: usize) -> usize {
     thread_rng().gen_range(min, max)
 }
 
-fn conversion_de_la_saisie_utilisateur(saisie: String) -> Option<usize> {
-    match usize::from_str(saisie.trim()) {
-        Ok(valeur) => Some(valeur),
-        Err(_) => None,
+fn conversion_de_la_saisie_utilisateur(saisie: Option<String>) -> Option<usize> {
+    if let Some(saisie_string) = saisie {
+        match usize::from_str(saisie_string.trim()) {
+            Ok(valeur) => Some(valeur),
+            Err(_) => None,
+        }
+    } else {
+        None
     }
 }
 
-fn recuperation_de_la_saisie_utilisateur() -> Option<usize> {
+fn recuperation_de_la_saisie_utilisateur() -> Option<String> {
     // Petit affichage
     print!("Valeur: ");
     match stdout().flush() {
@@ -29,7 +33,7 @@ fn recuperation_de_la_saisie_utilisateur() -> Option<usize> {
     // Récupération de la saisie
     let mut saisie_utilisateur = String::new();
     match stdin().read_line(&mut saisie_utilisateur) {
-        Ok(_) => conversion_de_la_saisie_utilisateur(saisie_utilisateur),
+        Ok(_) => Some(saisie_utilisateur),
         Err(e) => {
             eprintln!("Erreur de saisie utilisateur: {}", e);
             None
@@ -37,10 +41,12 @@ fn recuperation_de_la_saisie_utilisateur() -> Option<usize> {
     }
 }
 
-fn jeu(valeur_a_deviner: usize, mut nombre_de_coup: usize) {
-    while nombre_de_coup > 0 {
+fn jeu(valeur_a_deviner: usize, mut nombre_de_coup: isize) {
+    while nombre_de_coup >= 0 {
         println!("{} coups restant", nombre_de_coup);
-        if let Some(valeur_saisie) = recuperation_de_la_saisie_utilisateur() {
+        if let Some(valeur_saisie) =
+            conversion_de_la_saisie_utilisateur(recuperation_de_la_saisie_utilisateur())
+        {
             match valeur_a_deviner.cmp(&valeur_saisie) {
                 Ordering::Greater => println!("Plus grand"),
                 Ordering::Equal => {
@@ -54,9 +60,23 @@ fn jeu(valeur_a_deviner: usize, mut nombre_de_coup: usize) {
         }
         nombre_de_coup -= 1;
     }
+
+    if nombre_de_coup < 0 {
+        println!("Perdu le nombre à deviner était: {}", valeur_a_deviner);
+    }
 }
 
 fn main() {
-    let valeur_a_deviner = creation_de_la_valeur_a_deviner(0, 10);
-    jeu(valeur_a_deviner, 10);
+    loop {
+        let valeur_a_deviner = creation_de_la_valeur_a_deviner(0, 10);
+        jeu(valeur_a_deviner, 10);
+        if let Some(rejouer) = recuperation_de_la_saisie_utilisateur() {
+            match rejouer.trim() {
+                "O" => (),
+                _ => break,
+            }
+        } else {
+            panic!("Saisie invalide")
+        }
+    }
 }
